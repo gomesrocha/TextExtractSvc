2c0483da7193ab950b15510a444521239e05cc3e
Fabio Gomes ROcha <gomesrocha@gmail.com>
2021-05-01 11:55:04 -0300

    Create Maven Project with Spring - Java 11

---
+++ src/main/java/org/digitalse/TextExtractSvc/TextExtractSvcApplication.java
@@ -0,0 +1,13 @@
+package org.digitalse.TextExtractSvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextExtractSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextExtractSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/digitalse/TextExtractSvc/TextExtractSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.digitalse.TextExtractSvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextExtractSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a8aed5dea8e99d7938e411a95e01cada4e8f8428
Fabio Gomes Rocha <gomesrocha@gmail.com>
2021-05-04 14:51:44 -0300

    Controller Folha

--- src/main/java/org/digitalse/TextExtractSvc/model/FolhaController.java
+++ src/main/java/org/digitalse/TextExtractSvc/model/FolhaController.java
@@ -2,7 +2,6 @@ package org.digitalse.TextExtractSvc.model;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;

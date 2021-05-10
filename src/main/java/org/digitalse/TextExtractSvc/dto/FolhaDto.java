4881e6ee441095e10656c91d2dc7ead338750854
Fabio Gomes Rocha <gomesrocha@gmail.com>
2021-05-10 19:28:35 -0300

    Atualização Folha Pagamento

--- src/main/java/org/digitalse/TextExtractSvc/dto/FolhaDto.java
+++ src/main/java/org/digitalse/TextExtractSvc/dto/FolhaDto.java
@@ -1,30 +1,41 @@
 package org.digitalse.TextExtractSvc.dto;
 
 public class FolhaDto {
-	private float salario;
-	private float inss;
-	private float irrf;
-	private float salarioLiquido;
+	private double salario;
+	private double inss;
+	private double irrf;
+	private double salarioLiquido;
 	
-	public FolhaDto(float salario) {
+	public FolhaDto(double salario) {
 		this.salario = salario;
 		
 	}
-	public float getSalario() {
+	public double getSalario() {
 		return salario;
 	}
-	public void setSalario(float salario) {
+	public void setSalario(double salario) {
 		this.salario = salario;
 	}
-	public float getInss() {
+	public double getInss() {
+		if(this.salario < 1100.01) {
+			this.inss = this.salario * 0.075;
+		} else if(this.salario > 1100.00 && this.salario < 2203.46) {
+			this.inss = this.salario * 0.09;
+		} else if(this.salario > 2203.45 && this.salario < 3305.23) {
+			this.inss = this.salario * 0.12;
+		}else if(this.salario > 3305.22 && this.salario < 6433.57) {
+			this.inss = this.salario * 0.14;
+		}		else if(this.salario > 6433.57) {
+			this.inss = 6433.57 * 0.14;
+		}
 		return inss;
 	}
 	
-	public float getIrrf() {
+	public double getIrrf() {
 		return irrf;
 	}
 	
-	public float getSalarioLiquido() {
+	public double getSalarioLiquido() {
 		return salarioLiquido;
 	}
 

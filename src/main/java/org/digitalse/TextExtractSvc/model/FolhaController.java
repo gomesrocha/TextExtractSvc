8fc5793ff5c96181d2ea9f78b9a72dc249a20df4
Fabio Gomes Rocha <gomesrocha@gmail.com>
2021-05-10 19:16:14 -0300

    Atualização

--- src/main/java/org/digitalse/TextExtractSvc/model/FolhaController.java
+++ src/main/java/org/digitalse/TextExtractSvc/model/FolhaController.java
@@ -4,12 +4,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.digitalse.TextExtractSvc.dto.FolhaDto;
 @RestController
 @RequestMapping("/folha")
 public class FolhaController {
-	
 	
 	@GetMapping
 	public ResponseEntity<FolhaDto> calcular(@RequestParam float salario){

89ce48bc8309ff4a9981eff19fa3bf585d9e092a
Fabio Gomes Rocha <gomesrocha@gmail.com>
2021-05-10 20:50:21 -0300

    Nova funcionalidade de Token

---
+++ src/main/java/org/digitalse/TextExtractSvc/dto/TokenProbDto.java
@@ -0,0 +1,46 @@
+package org.digitalse.TextExtractSvc.dto;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
+import opennlp.tools.util.Span;
+
+public class TokenProbDto {
+	private String texto;
+	private InputStream is;
+	private double[] probs;
+	
+	public TokenProbDto(String texto) {
+		this.texto = texto;
+		try {
+			is = new FileInputStream("./data/pt-token.bin");
+			TokenizerModel tm = new TokenizerModel(is);
+			TokenizerME tk = new TokenizerME(tm);
+			Span tokens[] = tk.tokenizePos(texto);
+			probs = tk.getTokenProbabilities();
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public double[] getProbs() {
+		return probs;
+	}
+
+}
--- src/main/java/org/digitalse/TextExtractSvc/model/TokenController.java
+++ src/main/java/org/digitalse/TextExtractSvc/model/TokenController.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.digitalse.TextExtractSvc.dto.TokenDto;;
+import org.digitalse.TextExtractSvc.dto.TokenDto;
+import org.digitalse.TextExtractSvc.dto.TokenProbDto;
 
 @RestController
 @RequestMapping("/tokens")
@@ -14,6 +15,11 @@ public class TokenController {
 	@GetMapping
 	public ResponseEntity<TokenDto> TokenizeText(@RequestParam String texto) {
 		return ResponseEntity.ok(new TokenDto(texto));
+		
+	}
+	@GetMapping("/teste")
+	public ResponseEntity<TokenProbDto> TokenizeProbText(@RequestParam String texto) {
+		return ResponseEntity.ok(new TokenProbDto(texto));
 		
 	}
 

4cc702eef05d0351705cc2c635c5cb32645f7b85
Fabio Gomes Rocha <gomesrocha@gmail.com>
2021-05-25 10:26:38 -0300

    Atualização da funcionalidade de POSTagger

--- src/main/java/org/digitalse/TextExtractSvc/dto/TokenDto.java
+++ src/main/java/org/digitalse/TextExtractSvc/dto/TokenDto.java
@@ -1,17 +1,31 @@
 package org.digitalse.TextExtractSvc.dto;
 
+import org.digitalse.TextExtractSvc.model.PosTaggerMeRB;
+import org.digitalse.TextExtractSvc.model.TextPreProcessing;
+
 import opennlp.tools.tokenize.Tokenizer;
 import opennlp.tools.tokenize.WhitespaceTokenizer;
 
 public class TokenDto {
 	private String texto;
 	private String result[];
-	
+	private String[] resultTag;
+	private PosTaggerMeRB ptmr = null;
+	private TextPreProcessing tpp = null;
 	public TokenDto(String texto) {
-		this.texto = texto;
+		this.texto = texto.toLowerCase();
 		Tokenizer tk = WhitespaceTokenizer.INSTANCE;
-		this.result = tk.tokenize(texto);
+		tpp = new TextPreProcessing();
+		this.result = tk.tokenize(texto.toLowerCase());	
+		this.result = tpp.removeStopWords(this.result);
+		this.ptmr = new PosTaggerMeRB(result);
+		this.resultTag = ptmr.getResult();
 	}
+	
+	public String[] getResultTag() {
+		return resultTag;
+	}
+
 	public String[] getResult() {
 		return result;
 	}

3894e1d609174311a7ce250254b397d59ae0a8ac
Fabio Gomes Rocha <gomesrocha@gmail.com>
2021-05-31 13:51:40 -0300

    Atualização
    Nova funcionalidade

--- src/main/java/org/digitalse/TextExtractSvc/control/TextPreProcessing.java
+++ src/main/java/org/digitalse/TextExtractSvc/control/TextPreProcessing.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/preprocessing")
 public class TextPreProcessing {
-	
+		
 	@GetMapping
 	public String textProcess() {
 		return "Texto pré processado";
